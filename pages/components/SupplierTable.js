e3957be3e812f83214901ec2b014ee711e10689b
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-19 05:48:34 +0700

    add page

--- pages/components/SupplierTable.js
+++ pages/components/SupplierTable.js
@@ -1,6 +1,76 @@
+// import { useState } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+// function SupplierTable() {
+//   const [suppliers, setSuppliers] = useState([]);
+//   const [name, setName] = useState('');
+//   const [phoneNumber, setPhoneNumber] = useState('');
+//   const [address, setAddress] = useState('');
+
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     const newSupplier = { name, phoneNumber, address };
+//     setSuppliers([...suppliers, newSupplier]);
+//     setName('');
+//     setPhoneNumber('');
+//     setAddress('');
+//   }
+
+//   function handleDelete(index) {
+//     const newSuppliers = [...suppliers];
+//     newSuppliers.splice(index, 1);
+//     setSuppliers(newSuppliers);
+//   }
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <label>
+//           Supplier name:
+//           <input type="text" value={name} onChange={event => setName(event.target.value)} />
+//         </label>
+//         <label>
+//           Phone number:
+//           <input type="text" value={phoneNumber} onChange={event => setPhoneNumber(event.target.value)} />
+//         </label>
+//         <label>
+//           Address:
+//           <input type="text" value={address} onChange={event => setAddress(event.target.value)} />
+//         </label>
+//         <button type="submit" className="btn btn-primary ml-2">Add supplier</button>
+//       </form>
+//       <table className="table table-striped">
+//         <thead>
+//           <tr>
+//             <th>Name</th>
+//             <th>Phone number</th>
+//             <th>Address</th>
+//             <th>Actions</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {suppliers.map((supplier, index) => (
+//             <tr key={index}>
+//               <td>{supplier.name}</td>
+//               <td>{supplier.phoneNumber}</td>
+//               <td>{supplier.address}</td>
+//               <td><button onClick={() => handleDelete(index)}>Delete</button></td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// }
+
+// export default SupplierTable;
+
+
+
+import Link from 'next/link';
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 function SupplierTable() {
   const [suppliers, setSuppliers] = useState([]);
@@ -60,6 +130,11 @@ function SupplierTable() {
           ))}
         </tbody>
       </table>
+      <div className="text-center">
+        <Link href="/supplier-management">
+          <a className="btn btn-secondary mt-3">Go to Supplier Management</a>
+        </Link>
+      </div>
     </div>
   );
 }
