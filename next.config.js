0c531d3e85d06f662ad01b910ec29ffec5e627ae
MinKhantKyaw <118881845+MinKhantKyaw5006@users.noreply.github.com>
2023-03-19 05:57:23 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/suppliermanagement',
+        destination: '/suppliermanagement.js',
+      },
+    ];
+  },
+};
